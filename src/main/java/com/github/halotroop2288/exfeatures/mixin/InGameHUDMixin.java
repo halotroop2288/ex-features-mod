3874ce60e4114b0b33d49f028cb3f06c9e0731f7
halotroop2288 <carolinejoybell@gmail.com>
2020-01-09 02:41:13 -0800

    Added the permanent game version indicator

--- src/main/java/com/github/halotroop2288/exfeatures/mixin/InGameHUDMixin.java
+++ src/main/java/com/github/halotroop2288/exfeatures/mixin/InGameHUDMixin.java
@@ -1,18 +1,24 @@
 package com.github.halotroop2288.exfeatures.mixin;
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.*;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import net.minecraft.SharedConstants;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawableHelper;
 import net.minecraft.client.gui.hud.InGameHud;
 
 @Mixin(InGameHud.class)
 public class InGameHUDMixin extends DrawableHelper
 {
+	@Shadow MinecraftClient client;
+	
 	@Inject(at = @At("TAIL"), method = "render", remap = false)
 	public void render(CallbackInfo info)
 	{
-		
+		if (!this.client.options.debugEnabled)
+			this.client.textRenderer.draw("Minecraft " + SharedConstants.getGameVersion().getName(), 1.55F, 2F, 14737632); // CLOSE ENOUGH!
 	}
 }
--- src/main/java/com/github/halotroop2288/exfeatures/mixin/InventoryScreenMixin.java
+++ src/main/java/com/github/halotroop2288/exfeatures/mixin/InventoryScreenMixin.java
@@ -1,15 +1,19 @@
 package com.github.halotroop2288.exfeatures.mixin;
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.ingame.InventoryScreen;
 
 @Mixin(InventoryScreen.class)
 public class InventoryScreenMixin
 {
+	@Shadow MinecraftClient client;
+	
 	@Inject(at = @At("HEAD"), method = "init")
 	private void init(CallbackInfo info)
 	{ System.out.println("Successfully injected the Inventory Screen."); }
