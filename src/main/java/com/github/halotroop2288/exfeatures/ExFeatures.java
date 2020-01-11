bfbdf8df8fee7055d1fccbb22fc4d87578b15ab8
halotroop2288 <carolinejoybell@gmail.com>
2020-01-10 17:52:38 -0800

    Added disclaimers for WTFPL and removed IWG

--- src/main/java/com/github/halotroop2288/exfeatures/ExFeatures.java
+++ src/main/java/com/github/halotroop2288/exfeatures/ExFeatures.java
@@ -15,6 +15,11 @@ import net.minecraft.util.registry.Registry;
 import net.minecraft.world.biome.Biome.SpawnEntry;
 import net.minecraft.world.biome.Biomes;
 
+/* This program is free software. It comes without any warranty, to
+ * the extent permitted by applicable law. You can redistribute it
+ * and/or modify it under the terms of the Do What The Fuck You Want
+ * To Public License, Version 2, as published by Sam Hocevar. See
+ * http://www.wtfpl.net/ for more details. */
 public class ExFeatures implements ModInitializer
 {
 	public static final SpawnEntry GIANT_ZOMBIE_SPAWN = new SpawnEntry(EntityType.GIANT, 1, 0, 2);
