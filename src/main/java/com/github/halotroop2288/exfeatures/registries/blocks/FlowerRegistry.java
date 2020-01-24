012a794d36086bd8761337d675aecdbb7565259d
halotroop2288 <carolinejoybell@gmail.com>
2020-01-24 12:56:30 -0800

    Tried to make flowers spawn in the world.
    It doesn't seem like it's working, though the code looks right, and
    doesn't give any errors at all.

--- src/main/java/com/github/halotroop2288/exfeatures/registries/blocks/FlowerRegistry.java
+++ src/main/java/com/github/halotroop2288/exfeatures/registries/blocks/FlowerRegistry.java
@@ -1,11 +1,21 @@
 package com.github.halotroop2288.exfeatures.registries.blocks;
 
+import java.util.function.Consumer;
+
 import com.github.halotroop2288.exfeatures.registries.BlockRegistry;
 
 import net.fabricmc.fabric.api.block.FabricBlockSettings;
+import net.fabricmc.fabric.api.event.registry.RegistryEntryAddedCallback;
 import net.minecraft.block.*;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.item.*;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.world.biome.Biome;
+import net.minecraft.world.gen.GenerationStep;
+import net.minecraft.world.gen.feature.Feature;
+import net.minecraft.world.gen.feature.RandomPatchFeatureConfig;
+import net.minecraft.world.gen.placer.SimpleBlockPlacer;
+import net.minecraft.world.gen.stateprovider.WeightedStateProvider;
 
 public class FlowerRegistry
 {
@@ -18,10 +28,28 @@ public class FlowerRegistry
 		registerFlower("paeonia", PAEONIA);
 		registerFlower("rose", ROSE);
 		registerFlower("cyan_flower", CYAN_FLOWER);
+		forEveryBiome(FlowerRegistry::addFlowerFeature);
 	}
 	
 	private static void registerFlower(String id, Block block)
 	{
 		BlockRegistry.registerBlock(id, block, ItemGroup.DECORATIONS);
+	}
+
+	private static void forEveryBiome(Consumer<Biome> biomes)
+	{
+		Registry.BIOME.forEach(biomes);
+		RegistryEntryAddedCallback.event(Registry.BIOME).register((rawId, id, biome) -> biomes.accept(biome));
+	}
+	
+	private static void addFlowerFeature(Biome biome)
+	{
+		biome.addFeature(GenerationStep.Feature.VEGETAL_DECORATION, Feature.FLOWER.configure(
+			(new RandomPatchFeatureConfig.Builder((new WeightedStateProvider())
+				.addState(ROSE.getDefaultState(), 5)
+				.addState(PAEONIA.getDefaultState(), 5)
+				.addState(CYAN_FLOWER.getDefaultState(), 5),
+				new SimpleBlockPlacer())).tries(64).build())
+			);
 	}
 }
